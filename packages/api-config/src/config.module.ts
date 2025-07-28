e0c1ff0f4bbfdd751caf50e21c31671c79417dc1
tapuru <pogladtapira@gmail.com>
2025-07-28 18:12:33 +0500

    CRI-8 init(packages): init api-config package

---
+++ packages/api-config/src/config.module.ts
@@ -0,0 +1,20 @@
+import { ConfigService } from "@/config.service";
+import { DEFAULT_CONFIG } from "@/lib/default-config.const";
+import { Module } from "@nestjs/common";
+
+const configFactory = {
+  provide: ConfigService,
+  useFactory: () => {
+    const config = new ConfigService(DEFAULT_CONFIG);
+    config.loadFromEnv();
+    return config;
+  },
+};
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [configFactory],
+  exports: [configFactory],
+})
+export class ConfigModule {}
---
+++ packages/api-config/src/lib/config.type.ts
@@ -0,0 +1,17 @@
+import { Transport } from "@nestjs/microservices";
+import { Microservice } from "./microservices.enum";
+
+export interface MicroserviceConfig {
+  options: {
+    host: string;
+    port: number;
+  };
+  //TODO: find a way to include any transport type not only TCP
+  transport: Transport.TCP;
+}
+
+export interface ConfigData {
+  env: string;
+  port: number;
+  services: Record<Microservice, MicroserviceConfig>;
+}

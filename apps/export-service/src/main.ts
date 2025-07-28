a42ab36ed9764583f9a961e7a047766d5620861f
tapuru <pogladtapira@gmail.com>
2025-07-28 19:20:27 +0500

    CRI-8 feat(workspaces): add root env files handling, env.example, fix some apps packages name

--- apps/export-service/src/main.ts
+++ apps/export-service/src/main.ts
@@ -6,6 +6,12 @@ import {
   DEFAULT_CONFIG,
   Microservice,
 } from '@bc-arch-drafter/api-config';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+config({ path: resolve(process.cwd(), `../../.env.${NODE_ENV}`) });
 
 async function bootstrap() {
   const configService = new ConfigService(DEFAULT_CONFIG);
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+import { DEFAULT_CONFIG } from '@bc-arch-drafter/api-config';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+config({ path: resolve(process.cwd(), `../../.env.${NODE_ENV}`) });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.GATEWAY_PORT ?? DEFAULT_CONFIG.port;
+
+  await app.listen(PORT, () =>
+    console.log(`Gateway app started at port ${PORT}`),
+  );
 }
 bootstrap();

c72163fcb61e855a224a4507005ad116f74cb9a3
fireonex <mari.krulikovskaya@mail.ru>
2025-07-29 15:48:34 +0300

    feat: init web-client

---
+++ apps/web-client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+      <div className={'flex items-center justify-center'}>
+        <div className={'m-5 p-5 w-fit border-2 bg-mint-500 text-xl'}>
+          <p className={'text-shadow-lg'}>Here be dragons</p>
+        </div>
+      </div>
+    )
+}
+
+export default App

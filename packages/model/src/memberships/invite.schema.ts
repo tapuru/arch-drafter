7d630596b66168e142ffa25196a8e64f358d5cae
tapuru <pogladtapira@gmail.com>
2025-08-05 16:35:40 +0500

    refactor(model): refactor model package

--- packages/model/src/memberships/invite.schema.ts
+++ packages/model/src/memberships/invite.schema.ts
@@ -6,6 +6,8 @@ import { UserSchema } from '@/users';
 
 export const InviteIdSchema = BaseIdSchema.brand<'InviteId'>();
 export type InviteId = z.infer<typeof InviteIdSchema>;
+export const parseInviteId = (data: unknown) => InviteIdSchema.parse(data);
+export const isInviteId = (data: unknown): data is InviteId => InviteIdSchema.safeParse(data).success;
 
 export const InviteSchema = z.object({
   id: InviteIdSchema,
@@ -16,5 +18,6 @@ export const InviteSchema = z.object({
   acceptedAt: NullableIsoDateSchema,
   rejectedAt: NullableIsoDateSchema,
 });
-
 export type Invite = z.infer<typeof InviteSchema>;
+export const parseInvite = (data: unknown) => InviteSchema.parse(data);
+export const isInvite = (data: unknown): data is Invite => InviteSchema.safeParse(data).success;
--- packages/model/src/memberships/memebership.schema.ts
+++ packages/model/src/memberships/memebership.schema.ts
@@ -6,6 +6,8 @@ import { UserProjectRoleSchema, UserSchema } from '@/users';
 
 export const MembershipIdSchema = BaseIdSchema.brand<'MemebershipId'>();
 export type MemebershipId = z.infer<typeof MembershipIdSchema>;
+export const parseMembershipId = (data: unknown) => MembershipIdSchema.parse(data);
+export const isMembershipId = (data: unknown): data is MemebershipId => MembershipIdSchema.safeParse(data).success;
 
 export const MembershipSchema = z.object({
   id: MembershipIdSchema,
@@ -14,5 +16,6 @@ export const MembershipSchema = z.object({
   role: UserProjectRoleSchema,
   joinedAt: IsoDateSchema,
 });
-
 export type Membership = z.infer<typeof MembershipSchema>;
+export const parseMembership = (data: unknown) => MembershipSchema.parse(data);
+export const isMembership = (data: unknown): data is Membership => MembershipSchema.safeParse(data).success;

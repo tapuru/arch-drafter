8568d127d5d902291bdb73eeb09edbd4822636d9
tapuru <pogladtapira@gmail.com>
2025-08-08 20:03:18 +0500

    feat(lib): add send message function

---
+++ packages/lib/src/transport-utils/send-message.ts
@@ -0,0 +1,15 @@
+import { AppActions } from '@bc-arch-drafter/contracts';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+
+export const sendMessage = async <TAction extends keyof AppActions>({
+  client,
+  pattern,
+  payload,
+}: {
+  client: ClientProxy;
+  pattern: { cmd: TAction };
+  payload: AppActions[TAction]['request'];
+}): Promise<AppActions[TAction]['response']> => {
+  return lastValueFrom(client.send<AppActions[TAction]['response'], AppActions[TAction]['request']>(pattern, payload));
+};

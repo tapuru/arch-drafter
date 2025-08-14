8bdf6c9502566762109414871b52c9a1d8540311
tapuru <pogladtapira@gmail.com>
2025-08-14 19:43:02 +0500

    feat(postgres-db): add idColumn and default page size const

--- packages/postgres-db/src/shared/index.ts
+++ packages/postgres-db/src/shared/index.ts
@@ -1,2 +1,5 @@
 export * from './custom-types.schema';
 export * from './columns.schema';
+export { buildOrderBy } from './build-order-by';
+
+export { DEFAULT_PAGE_SIZE } from './consts/default-page-size.const';

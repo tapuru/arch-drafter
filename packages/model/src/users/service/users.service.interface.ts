c6892d8c75a9899c5517f412a90c57fb0584822a
DePacos <depacos@yandex.ru>
2025-08-26 23:21:16 +0300

    feat(model): user interface & schema for update & delete user

--- packages/model/src/users/service/users.service.interface.ts
+++ packages/model/src/users/service/users.service.interface.ts
@@ -1,7 +1,16 @@
-import { User } from '@/users';
+import { Password, User } from '@/users';
 
 export type GetUserById = (id: User['id']) => Promise<User>;
 
+export type UpdateUserById = (
+  id: User['id'],
+  data: Partial<Pick<User, 'name'> & { password: Password }>,
+) => Promise<User>;
+
+export type DeleteUserById = (id: User['id']) => Promise<void>;
+
 export interface UserService {
   getUserById: GetUserById;
+  updateUserById: UpdateUserById;
+  deleteUserById: DeleteUserById;
 }

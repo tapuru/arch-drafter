6ed00c3fd370d899d832f8fc40b0b6aa809bf2e5
tapuru <pogladtapira@gmail.com>
2025-08-26 15:45:28 +0500

    feat(contracts): add invites routes

--- packages/contracts/src/routes/api-routes.const.ts
+++ packages/contracts/src/routes/api-routes.const.ts
@@ -1,5 +1,12 @@
 export const API_ROUTES = {
   PROJECTS: '/projects',
+  INVITES: {
+    ROOT: '/invites',
+    GET_PROJECT_INVITES: (projectId: string) => `/projects/${projectId}/invites`,
+    GET_USER_INVITES: (userId: string) => `/users/${userId}/invites`,
+    ACCEPT: '/invites/accept',
+    REJECT: '/invites/reject',
+  },
   AUTH: {
     ROOT: '/auth',
     ME: '/me',

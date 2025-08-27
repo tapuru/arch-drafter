277548b3855ce53dd5f647f64cfabe1361c1fd4c
DePacos <depacos@yandex.ru>
2025-08-27 20:02:10 +0300

    feat(gateway): user update & delete method

--- apps/gateway/src/users/users.controller.ts
+++ apps/gateway/src/users/users.controller.ts
@@ -1,8 +1,14 @@
 import { Microservice } from '@bc-arch-drafter/api-config';
-import { API_ROUTES, GetUserByIdResponseDto } from '@bc-arch-drafter/contracts';
-import { sendMessage } from '@bc-arch-drafter/lib';
+import {
+  API_ROUTES,
+  GetUserByIdResponseDto,
+  UpdateUserRequestSchema,
+  UpdateUserRequestDto,
+  UpdateUserResponseDto,
+} from '@bc-arch-drafter/contracts';
+import { sendMessage, ZodValidationPipe } from '@bc-arch-drafter/lib';
 import { parseUserId } from '@bc-arch-drafter/model';
-import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, Patch } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
 @Controller(API_ROUTES.USERS)
@@ -14,6 +20,27 @@ export class UsersController {
     return await sendMessage({
       client: this.client,
       pattern: { cmd: 'users.get-by-id' },
+      payload: { id: parseUserId(id) },
+    });
+  }
+
+  @Patch('/:id')
+  async updateUserById(
+    @Param('id') id: string,
+    @Body(new ZodValidationPipe(UpdateUserRequestSchema.shape.data)) data: UpdateUserRequestDto['data'],
+  ): Promise<UpdateUserResponseDto> {
+    return await sendMessage({
+      client: this.client,
+      pattern: { cmd: 'users.update-by-id' },
+      payload: { id: parseUserId(id), data },
+    });
+  }
+
+  @Delete('/:id')
+  async deleteUserById(@Param('id') id: string) {
+    return await sendMessage({
+      client: this.client,
+      pattern: { cmd: 'users.delete-by-id' },
       payload: { id: parseUserId(id) },
     });
   }

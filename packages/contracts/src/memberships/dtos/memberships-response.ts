4408c5ea166432cd4e35f2e74ca66cc12d94f4b9
tapuru <pogladtapira@gmail.com>
2025-09-13 19:14:42 +0500

    fix(contracts): fix response type

--- packages/contracts/src/memberships/dtos/memberships-response.ts
+++ packages/contracts/src/memberships/dtos/memberships-response.ts
@@ -1,15 +1,15 @@
 import { GetAllResSchema, MembershipSchema, ProjectSchema } from '@bc-arch-drafter/model';
 import z from 'zod';
 
-import { ApiResponseSchema } from '@/responses';
+import { ApiResponseSchema, ApiSuccessResponseSchema } from '@/responses';
 
-export const OneMembershipResponseSchema = ApiResponseSchema(MembershipSchema);
+export const OneMembershipResponseSchema = ApiSuccessResponseSchema(MembershipSchema);
 export type OneMembershipResponseDto = z.infer<typeof OneMembershipResponseSchema>;
 export const parseOneMembershipResponse = (data: unknown) => OneMembershipResponseSchema.parse(data);
 export const isOneMembershipResponse = (data: unknown): data is OneMembershipResponseDto =>
   OneMembershipResponseSchema.safeParse(data).success;
 
-export const ManyMembershipResponseSchema = ApiResponseSchema(
+export const ManyMembershipResponseSchema = ApiSuccessResponseSchema(
   GetAllResSchema(MembershipSchema.extend({ project: ProjectSchema })),
 );
 export type ManyMembershipResponseDto = z.infer<typeof ManyMembershipResponseSchema>;

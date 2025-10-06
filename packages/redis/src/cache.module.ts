f27aebabeffe1847fcf137944a037f4c93ad5e31
tapuru <pogladtapira@gmail.com>
2025-10-06 20:20:22 +0500

    fix(redis): add config module import, add utils reexports

--- packages/redis/src/cache.module.ts
+++ packages/redis/src/cache.module.ts
@@ -1,11 +1,13 @@
-import { ConfigService } from '@bc-arch-drafter/api-config';
+import { ConfigModule, ConfigService } from '@bc-arch-drafter/api-config';
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
   imports: [
+    ConfigModule,
     CacheModule.registerAsync({
+      imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (config: ConfigService) => {
         const { cacheDb, host, port } = config.get().redis;
--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -1,2 +1,4 @@
 export { RedisCacheModule } from './cache.module';
 export { RedisModule } from './redis.module';
+
+export { CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';

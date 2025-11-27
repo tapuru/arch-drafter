768e2947a85c8eb4f9f7efed870d05728e2981e7
fireonex <mari.krulikovskaya@mail.ru>
2025-11-27 12:42:49 +0300

    CRI-34 feat: add ui component alert-dialog

--- apps/web-client/src/features/auth/forgot-password/ui/forgot-password-form.tsx
+++ apps/web-client/src/features/auth/forgot-password/ui/forgot-password-form.tsx
@@ -2,15 +2,30 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router';
 
 import { WEB_ROUTES } from '@bc-arch-drafter/contracts';
-import { Button, Card, CardContent, CardDescription, CardHeader, CardTitle, Input } from '@bc-arch-drafter/ui';
+import {
+  ActionConfirmation,
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  Input,
+} from '@bc-arch-drafter/ui';
 
 export const ForgotPasswordForm = () => {
   const [isSendingLink, setIsSendingLink] = useState(false);
 
   const navigate = useNavigate();
 
+  const mockEmail = 'epam@epam.com';
+
   const sendLinkHandler = () => {
     setIsSendingLink(true);
+  };
+
+  const handleConfirm = () => {
+    setIsSendingLink(false);
   };
 
   const toLoginForm = () => {
@@ -18,34 +33,43 @@ export const ForgotPasswordForm = () => {
   };
 
   return (
-    <Card className="w-full max-w-md shadow-2xl">
-      <CardHeader>
-        <CardTitle className="text-2xl">Forgot Password</CardTitle>
-        <CardDescription>Enter your email address to receive a password reset link</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="space-y-4">
-          <div className="space-y-2">
-            <label htmlFor="email" className="block text-sm font-medium">
-              Email
-            </label>
-            <Input id="email" type="email" placeholder="your@email.com" autoComplete="email" />
-          </div>
-          {isSendingLink && (
-            <div className="mt-4 text-center text-sm">
-              The link has been sent by email. If you don’t receive an email send link again
+    <>
+      <Card className="w-full max-w-md shadow-2xl">
+        <CardHeader>
+          <CardTitle className="text-2xl">Forgot Password</CardTitle>
+          <CardDescription>Enter your email address to receive a password reset link</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <label htmlFor="email" className="block text-sm font-medium">
+                Email
+              </label>
+              <Input id="email" type="email" placeholder="your@email.com" autoComplete="email" />
             </div>
-          )}
-          <Button className="w-full" onClick={sendLinkHandler}>
-            Send Reset Link {isSendingLink && 'Again'}
-          </Button>
-          <div className="mt-4 text-center text-sm">
-            <a onClick={toLoginForm} className="text-primary underline cursor-pointer">
-              Back to Sign in
-            </a>
+            {isSendingLink && (
+              <div className="mt-4 text-center text-sm">
+                The link has been sent by email. If you don’t receive an email send link again
+              </div>
+            )}
+            <Button className="w-full" onClick={sendLinkHandler}>
+              Send Reset Link {isSendingLink && 'Again'}
+            </Button>
+            <div className="mt-4 text-center text-sm">
+              <a onClick={toLoginForm} className="text-primary underline cursor-pointer">
+                Back to Sign in
+              </a>
+            </div>
           </div>
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+      <ActionConfirmation
+          isOpen={isSendingLink}
+          setIsOpen={setIsSendingLink}
+          onConfirm={handleConfirm}
+          title="Email sent"
+          message={`We have sent a link to confirm your email to ${mockEmail}`}
+      />
+    </>
   );
 };
--- packages/ui/src/components/ui/index.ts
+++ packages/ui/src/components/ui/index.ts
@@ -1,7 +1,12 @@
+//shadcn-components
 export * from './button';
 export * from './card';
 export * from './checkbox';
 export * from './input';
 export * from './switch';
 export * from './skeleton';
+export * from './alert-dialog';
+
+//custom components based on shadcn
+export * from "./action-confirmation"
 
